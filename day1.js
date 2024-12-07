9217cab20069c74b4cf97cf5cff29a83599b734f
devdong9897 <dev.dong9897@gmail.com>
2024-12-07 15:25:54 +0900

    feat: 두 수의 곱 구하기

--- day1.js
+++ day1.js
@@ -1,15 +1,24 @@
 // 두 수의 합
-function solution(num1, num2) {
+function solution1(num1, num2) {
   return num1 + num2;
 }
 
-solution(2, 3);
-solution(100, 2);
+solution1(2, 3);
+solution1(100, 2);
 
 // 두 수의 차
-function solution(num1, num2) {
+function solution2(num1, num2) {
   return num1 - num2;
 }
 
-solution(2, 3);
-solution(100, 2);
+solution2(2, 3);
+solution2(100, 2);
+
+// 두 수의 곱
+function solution3(num1, num2) {
+  let answer = num1 * num2;
+  return answer;
+}
+
+solution3(3, 4);
+solution3(27, 19);
