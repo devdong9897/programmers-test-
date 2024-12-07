c48bf8c1302629787990c13aef8bf2f5df3045e3
devdong9897 <dev.dong9897@gmail.com>
2024-12-07 15:28:51 +0900

    feate: 숫자 비교하기

--- day2.js
+++ day2.js
@@ -1,7 +1,7 @@
 // day2: 사칙연산, 조건문, 배열
 
 // 두 수의 나눗셈
-function solution(num1, num2) {
+function solution1(num1, num2) {
   let answer = Math.floor((num1 / num2) * 1000);
   return answer;
 }
@@ -9,3 +9,16 @@ function solution(num1, num2) {
 solution(3, 2);
 solution(7, 3);
 solution(1, 16);
+
+// 숫자 비교하기
+function solution2(num1, num2) {
+  if (num1 === num2) {
+    return 1;
+  } else {
+    return -1;
+  }
+}
+
+solution(2, 3);
+solution(11, 11);
+solution(7, 99);
